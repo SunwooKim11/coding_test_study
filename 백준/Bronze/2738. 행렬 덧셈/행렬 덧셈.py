f1067ed895c18bd85ae23bf5bbfa898a24f4bd48
KIM SUNWOO <65994072+SunwooKim11@users.noreply.github.com>
2025-01-01 22:47:26 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,8 @@
+N, M = map(int, input().split())
+
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+C = [ [A[i][j] + B[i][j] for j in range(M)] for i in range(N)]
+
+for c in C:
+    print(*c, sep=' ')
